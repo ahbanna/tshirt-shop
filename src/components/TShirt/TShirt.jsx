effde00e214f32a95b7cbfac364c613fd5920976
Al Hasanul Banna <tutul5800@gmail.com>
2023-04-07 01:10:17 +0600

    add product details

---
+++ src/components/TShirt/TShirt.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./TShirt.css";
+
+const TShirt = ({ tshirt, handleAddToCart }) => {
+  const { picture, name, price } = tshirt;
+  return (
+    <div className="single-tshirt">
+      <img src={picture} alt="" />
+      <h3>{name}</h3>
+      <h5>Price: ${price}</h5>
+      <button onClick={handleAddToCart}>Buy Now</button>
+    </div>
+  );
+};
+
+export default TShirt;
