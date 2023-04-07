3a1a7dd8b66b168a63415d57e1952498a9cee148
Al Hasanul Banna <tutul5800@gmail.com>
2023-04-08 01:03:11 +0600

    add conditional rendering and css

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,22 @@
 import React from "react";
+import "./Cart.css";
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
   // console.log(cart);
+
+  let message;
+  if (cart.length === 0) {
+    message = <p className="red">Please add some products</p>;
+  } else {
+    message = <p className="green">Thanks for adding product</p>;
+  }
+
   return (
     <div>
       <h2>Order summary</h2>
       <h4>Total items: {cart.length}</h4>
+      {message}
+      {cart.length > 2 ? "Thanks for adding more than two items" : ""}
       {/* cart is as array, so we need map */}
       {cart.map((shirt) => (
         <h5 key={shirt._id}>
